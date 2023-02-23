a3dfa383ada848b5f6b8242313de376a94cea87c
DamirLut <d.amlutstudio@gmail.com>
2023-02-23 10:42:56 +0000

    README

--- src/entities/Discipline.ts
+++
@@ -1,8 +0,0 @@
-import { Field } from "../lib/DatabaseUtils";
-import { Entity } from "./Entity";
-
-export class Discipline extends Entity {
-  static override definition = [Field("name", "VARCHAR(128)")];
-
-  name: string;
-}
--- src/entities/Semester.ts
+++
@@ -1,8 +0,0 @@
-import { Field } from "../lib/DatabaseUtils";
-import { Entity } from "./Entity";
-
-export class Semester extends Entity {
-  static override definition = [Field("year", "YEAR")];
-
-  year: number;
-}
